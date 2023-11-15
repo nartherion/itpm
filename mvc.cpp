3b3d9528a9e1ee053ab62d769d3812fc5ae29e9c
Volodymyr Sachko <volodymyrsachko1@gmail.com>
2023-11-15 23:17:43 +0200

    added mvc example

---
+++ mvc.cpp
@@ -0,0 +1,46 @@
+#include <cstdlib>
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "mvc example");
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("guts.png"))
+    {
+        std::cerr << "failed to load the image" << '\n';
+        return EXIT_FAILURE;
+    }
+
+    sf::Sprite image;
+    image.setTexture(texture);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == sf::Mouse::Left)
+                    image.setScale(1.5, 1.5);
+                else
+                    image.setScale(1, 1);
+            }
+        }
+
+        window.clear(sf::Color::Black);
+
+        window.clear();
+        window.draw(image);
+
+        window.display();
+    }
+
+    return 0;
+}
